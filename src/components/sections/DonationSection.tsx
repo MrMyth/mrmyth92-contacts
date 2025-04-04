ebea8d8b12e862797c980469c7b6798039357d74
MrMyth <starchikov92@mail.ru>
2025-04-04 13:28:11 +0300

    Update DonationSection.tsx

--- src/components/sections/DonationSection.tsx
+++ src/components/sections/DonationSection.tsx
@@ -1,4 +1,3 @@
-
 import React from "react";
 import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
@@ -47,7 +46,7 @@ const DonationSection = () => {
             className="block w-full"
           >
             <Button className="w-full gaming-button bg-[#F15B3D] hover:bg-[#F15B3D]/80 text-white border-0" variant="outline">
-              <DollarSign className="mr-2 h-4 w-4 text-white" />
+              <Gift className="mr-2 h-4 w-4 text-white" />
               Поддержать на Boosty
             </Button>
           </a>
@@ -59,7 +58,7 @@ const DonationSection = () => {
             className="block w-full"
           >
             <Button className="w-full gaming-button bg-[#6366F1] hover:bg-[#6366F1]/80 text-white border-0" variant="outline">
-              <DollarSign className="mr-2 h-4 w-4 text-white" />
+              <Gift className="mr-2 h-4 w-4 text-white" />
               Поддержать на Donatty
             </Button>
           </a>
@@ -71,7 +70,7 @@ const DonationSection = () => {
             className="block w-full"
           >
             <Button className="w-full gaming-button bg-[#F59E0B] hover:bg-[#F59E0B]/80 text-white border-0" variant="outline">
-              <DollarSign className="mr-2 h-4 w-4 text-white" />
+              <Gift className="mr-2 h-4 w-4 text-white" />
               Поддержать через Юмани
             </Button>
           </a>
